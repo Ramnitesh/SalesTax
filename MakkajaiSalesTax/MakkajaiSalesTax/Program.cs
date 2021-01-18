4cac3aebf3ae57cba7b423e5698cf14390bc7173
Ramnitesh <ramnitesh.saran@gmail.com>
2021-01-18 23:52:27 +0530

    Project Added 1

---
+++ MakkajaiSalesTax/MakkajaiSalesTax/Program.cs
@@ -0,0 +1,24 @@
+﻿using MakkajaiSalesTax.Interface;
+using MakkajaiSalesTax.Models;
+using MakkajaiSalesTax.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace MakkajaiSalesTax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ISalesTax isalesTax = SalesTaxImplementation.getApi();
+                isalesTax.countSalesTax();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
